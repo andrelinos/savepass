a4cefb7ceeb3b757ef32c4f1b2794f7abcb59472
Andrelino Silva <andrelinocom@gmail.com>
2021-10-31 15:34:00 -0300

    End: Terminado desafio - test success

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,85 +1,76 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 
-
 import {
-  Container,
-  AboutUser,
-  Avatar,
-  TextContainer,
-  HelloMessage,
-  BoldText,
-  SecondaryMessage,
-  AddButton,
-  Icon,
-  BackButton,
-  Title,
+    Container,
+    AboutUser,
+    Avatar,
+    TextContainer,
+    HelloMessage,
+    BoldText,
+    SecondaryMessage,
+    AddButton,
+    Icon,
+    BackButton,
+    Title
 } from './styles';
 
 interface HeaderProps {
-  user?: {
-    name: string;
-    avatar_url: string;
-  }
+    user?: {
+        name: string;
+        avatar_url: string;
+    };
 }
 
 export function Header({ user }: HeaderProps) {
-  const { navigate, goBack } = useNavigation();
+    const { navigate, goBack } = useNavigation();
 
-  function handleAddPass() {
-    navigate('RegisterLoginData');
-  }
+    function handleAddPass() {
+        navigate('RegisterLoginData');
+    }
 
-  return (
-    <Container
-      hasUserData={!!user}
-      style={{
-        ...(user
-          ? {
-            backgroundColor: '#1967FB'
-          }
-          : {
-            backgroundColor: '#FFFFFF'
-          })
-      }}
-    >
-      {user ? (
-        <>
-          <AboutUser>
-            <Avatar source={{ uri: user.avatar_url }} />
+    return (
+        <Container
+            hasUserData={!!user}
+            style={{
+                ...(user
+                    ? {
+                          backgroundColor: '#1967FB'
+                      }
+                    : {
+                          backgroundColor: '#FFFFFF'
+                      })
+            }}
+        >
+            {user ? (
+                <>
+                    <AboutUser>
+                        <Avatar source={{ uri: user.avatar_url }} />
 
-            <TextContainer>
-              <HelloMessage>
-                Olá, <BoldText>{user.name}</BoldText>
-              </HelloMessage>
+                        <TextContainer>
+                            <HelloMessage>
+                                Olá, <BoldText>{user.name}</BoldText>
+                            </HelloMessage>
 
-              <SecondaryMessage>
-                Sinta-se seguro aqui
-              </SecondaryMessage>
-            </TextContainer>
-          </AboutUser>
+                            <SecondaryMessage>
+                                Sinta-se seguro aqui
+                            </SecondaryMessage>
+                        </TextContainer>
+                    </AboutUser>
 
-          <AddButton onPress={handleAddPass}>
-            <Icon
-              name="plus"
-              color="#FFFFFF"
-              size={24}
-            />
-          </AddButton>
-        </>
-      ) : (
-        <>
-          <BackButton onPress={goBack}>
-            <Icon
-              name="chevron-left"
-              color="#1967FB"
-              size={28}
-            />
-          </BackButton>
+                    <AddButton onPress={handleAddPass}>
+                        <Icon name="plus" color="#FFFFFF" size={24} />
+                    </AddButton>
+                </>
+            ) : (
+                <>
+                    <BackButton onPress={goBack}>
+                        <Icon name="chevron-left" color="#1967FB" size={28} />
+                    </BackButton>
 
-          <Title>Cadastro de senha</Title>
-        </>
-      )}
-    </Container>
-  );
+                    <Title>Cadastro de senha</Title>
+                </>
+            )}
+        </Container>
+    );
 }
--- src/screens/RegisterLoginData/styles.ts
+++ src/screens/RegisterLoginData/styles.ts
@@ -2,11 +2,11 @@ import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
-  flex: 1;
-  background-color: #F2F3F5;
-  padding: 0 ${RFValue(24)}px;
+    flex: 1;
+    background-color: #f2f3f5;
+    padding: 0 ${RFValue(24)}px;
 `;
 
 export const Form = styled.View`
-  margin-top: ${RFValue(24)}px;
+    margin-top: ${RFValue(24)}px;
 `;
